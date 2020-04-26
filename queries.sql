e12f7d81eef48b54ac5ed85d7c5b2dbe0d2c4e07
busy0312 <abbymacbook@Abbys-MacBook-Pro.local>
2020-04-26 01:54:44 -0500

    formatting

--- queries.sql
+++ queries.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS texas_crime;
+DROP TABLE IF EXISTS "Texas_Hate_Crimes";
 
-CREATE TABLE texas_crime (
+CREATE TABLE "Texas_Hate_Crimes" (
 	"Incident_ID" INT PRIMARY KEY NOT NULL,
 	"Incident_Date" Date NOT NULL,
 	"Year" INT NOT NULL,
@@ -23,16 +23,16 @@ CREATE TABLE texas_crime (
 
 DROP TABLE IF EXISTS "Austin_Hate_Crimes";
 CREATE TABLE "Austin_Hate_Crimes" (
-    "Incident_ID" VARCHAR  PRIMARY KEY NOT NULL,
-    "Incident_Date" DATE NOT NULL,
-    "Month" INT NOT NULL,
+	"Incident_ID" VARCHAR  PRIMARY KEY NOT NULL,
+	"Incident_Date" DATE NOT NULL,
+	"Month" INT NOT NULL,
 	"Day" INT NOT NUll,
 	"Year" INT NOT NULL,
-    "Day_of_Week" VARCHAR NOT NULL,
-    "Offense_Name" VARCHAR NOT NULL,
-    "Offender_Race" VARCHAR NOT NULL,
-    "Bias" VARCHAR NOT NULL,
-    "Victim_Type" VARCHAR,
+	"Day_of_Week" VARCHAR NOT NULL,
+	"Offense_Name" VARCHAR NOT NULL,
+	"Offender_Race" VARCHAR NOT NULL,
+	"Bias" VARCHAR NOT NULL,
+	"Victim_Type" VARCHAR,
 	"Offense_Location" VARCHAR NOT NULL,
 	"Total_Victims" INT NOT NULL,
 	"Total_Offenders" INT NOT NULL
